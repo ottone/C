8e44f6089373e76db75074b7d138f84c3727a6ed
Francesco <demonerosso77@gmail.com>
2018-12-25 13:28:09 +0100

    helloworld

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Francesco Parrucci on 25/12/18.
+//  Copyright © 2018 Francesco Parrucci. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
